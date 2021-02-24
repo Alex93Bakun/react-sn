122655fd6beda5872349a0b4af627405df023ce8
Alex93Bakun <bakun.alex93@gmail.com>
2021-02-24 16:45:09 +0200

    Dialogs component init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 import './App.scss';
 
@@ -11,7 +12,8 @@ const App = () => {
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+            {/*<Profile/>*/}
+            <Dialogs/>
         </div>
     );
 }
