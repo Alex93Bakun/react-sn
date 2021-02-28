1bb570780407b9c321f7071e2ddbdd63a7b95a98
Alex93Bakun <bakun.alex93@gmail.com>
2021-02-28 16:37:06 +0200

    stateCopy in reducers

--- src/index.js
+++ src/index.js
@@ -9,25 +9,16 @@ import store from "./redux/redux-store";
 
 import './index.css';
 
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
-let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <App/>
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
-});
 
 reportWebVitals();
