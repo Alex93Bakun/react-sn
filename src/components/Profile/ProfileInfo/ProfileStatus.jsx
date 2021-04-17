a4c8cef4f5a2f9e08d9dcd14e7b8ac26c7c29cef
Alex93Bakun <bakun.alex93@gmail.com>
2021-04-17 15:52:59 +0300

    ProfileStatus editMode

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -5,17 +5,36 @@ class ProfileStatus extends React.Component {
     editMode: false,
   };
 
+  activateEditMode() {
+    this.setState({
+      editMode: true,
+    });
+  }
+
+  deactivateEditMode() {
+    this.setState({
+      editMode: false,
+    });
+  }
+
   render() {
     return (
       <div>
         {!this.state.editMode && (
           <div>
-            <span>{this.props.status}</span>
+            <span onDoubleClick={this.activateEditMode.bind(this)}>
+              {this.props.status}
+            </span>
           </div>
         )}
         {this.state.editMode && (
           <div>
-            <input type="text" value={this.props.status} />
+            <input
+              autoFocus={true}
+              onBlur={this.deactivateEditMode.bind(this)}
+              type="text"
+              value={this.props.status}
+            />
           </div>
         )}
       </div>
